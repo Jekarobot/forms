6b22d6b00236550a838cb80e4b8b83ed889b9b5b
Jeka_Robot <petryakov.evgeni@gmail.com>
2024-07-23 19:21:07 +0300

    Всё готово

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  testEnvironment: 'jsdom',
+  testEnvironment: 'jest-environment-jsdom',
   silent: false,
   moduleNameMapper: {
     '\\.(css|less|scss|sss|styl)$': 'identity-obj-proxy',
